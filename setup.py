de8252ae5bf9f8016e60da66dcb1810ee064bee1
Rufis72 <milo.games.silverstein@gmail.com>
2025-12-30 10:38:25 -0800

    Bump version number to 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mangadl',
-    version='1.0.0',
+    version='1.0.1',
     description='A simple cli for scraping and formatting manga.',
     author='Rufis72',
     license='MIT',
